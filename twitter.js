d6e647f310db2659d5e3f0c2396f65a417baa126
Isaac <isaac.sanchez@mycit.ie>
2017-03-28 00:50:38 +0100

    tw

---
+++ twitter.js
@@ -0,0 +1,65 @@
+var rate   = 15000;
+var _count  = 1;
+var config = require('./config');
+var sent   = require('./sentiment');
+var Twit   = require('twit');
+var twits  = [];
+var T      = new Twit(config);
+
+
+exports.loadData = function(res, req) {
+  sent.loadData()
+}
+
+exports.getAllTwits = function(res, req) {
+  sent.getAll(res, req);
+}
+
+exports.getTwitsSearch = function(res, search, lang, count) {
+
+  T.get('search/tweets', {
+    q: search,
+    lang: lang ? lang : "en",
+    count: count ? count : _count,
+    result_type: "recent"
+  }, function (err, data, response) {
+
+    twits = [];
+    for (var t in data.statuses) {
+
+      twits.push({
+        "text" : data.statuses[t].text.trim(),
+        "id" : data.statuses[t].id,
+        "location" : data.statuses[t].user.location
+      });
+      // twits.push(data.statuses[t].text.trim());
+    }
+
+    sent.getSentiment(res, search, twits);
+  });
+}
+
+exports.removeTwitsSearch = function(res, search) {
+  sent.removeTopicResults(res, search);
+}
+
+// exports.getTwitsTrending = function() {
+
+//   T.get('trends/closest', {
+//     lat: 51.8969,
+//     long: 8.4863
+//   }, function (err, data, response) {
+
+//     console.log("Trending:\n" + JSON.stringify(data, null));
+
+//     for (var t in data.statuses) {
+//       // twits.push({
+//       //   "text" : data.statuses[t].text,
+//       //   "location" : data.statuses[t].user.location
+//       // });
+//       twits.push(data.statuses[t].text.trim());
+//     }
+
+//     sent.getSentiment(res, search, twits);
+//   });
+// }
