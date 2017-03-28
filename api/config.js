eaeb3aa51f7da6029fa50e3e24bb1d08671e2431
Isaac <isaac.sanchez@mycit.ie>
2017-03-28 18:37:00 +0100

    *

--- api/config.js
+++
@@ -1,7 +0,0 @@
-module.exports = {
-  consumer_key:         'YVSEGPaUbmFz2LhY8y0jn22qJ',
-  consumer_secret:      'gN0KSUpYmJ3KYIuXnf3mEGwisLSbSAxBHjcnIv9NDSv0rklP23',
-  access_token:         '827055257297510400-HS7TQ0zSq2Tp0MAv8iDm9sfsY8sVFZg',
-  access_token_secret:  'sKJ2SoeyqUKdA9NNjZgAeNLoxJRcA8I9MBx8UTF3Z2YQO',
-  timeout_ms:           60*1000  // optional HTTP request timeout to apply to all requests.
-}
