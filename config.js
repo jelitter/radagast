2756075750ff2660d77726b508a31853c8f1819e
Isaac <isaac.sanchez@mycit.ie>
2017-03-28 18:22:00 +0100

    deleted config.js

--- config.js
+++
@@ -1,7 +0,0 @@
-module.exports = {
-  consumer_key:         '<CONSUMER_KEY HERE>',
-  consumer_secret:      '<CONSUMER_SECRET HERE>',
-  access_token:         '<ACCESS_TOKEN HERE>',
-  access_token_secret:  '<ACCESS_TOKEN_SECRET HERE>',
-  timeout_ms:           60*1000  // optional HTTP request timeout to apply to all requests.
-}
