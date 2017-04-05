1428555f23711c33cf7b3f64bd5bc5084bbafbdc
Isaac Sanchez <isanchez@blizzard.com>
2017-04-05 09:23:40 +0100

    Changed word split

--- public/script2.js
+++ public/script2.js
@@ -93,7 +93,7 @@ function dataReceived(data) {
 function wordcloud(text) {
 
 	var font_multiplier = 10;
-    text = text.split(/\W/);
+    text = text.split(/[\s\.\,\?\!]/);
 
     var wordfreqs = {};
     var higher = 0;
